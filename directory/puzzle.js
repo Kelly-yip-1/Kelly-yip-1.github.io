4e257ef702bf9e3cd26b6d4d9184033ebe194095
Kelly-yip-1 <79957777+Kelly-yip-1@users.noreply.github.com>
2021-05-19 22:19:57 +0800

    Add files via upload

---
+++ directory/puzzle.js
@@ -0,0 +1,35 @@
+function showTopic1(){
+  var x=document.getElementById('demo1');
+  x.style.color="green";
+  alert("Congratulations! Your answer is right!");
+}
+function showTopic2(){
+  var x=document.getElementById('demo2');
+  x.style.color="red";
+  alert("Wrong answer, please try again!");
+}
+function showTopic3(){
+  var x=document.getElementById('demo3');
+  x.style.color="red";
+  alert("Wrong answer, please try again!");
+}
+function showTopic4(){
+  var x=document.getElementById('demo4');
+  x.style.color="red";
+  alert("Wrong answer, please try again!");
+}
+
+function showTopic5(){
+  alert("Congratulations! Your answer is right!");
+}
+
+function showTopic6(){
+  alert("Wrong answer, please try again!");
+}
+
+function showTopic7(){
+  alert("wrong answer,please try again!")
+}
+function showTopic8(){
+  alert("Congratulations! Your answer is right!")
+}
